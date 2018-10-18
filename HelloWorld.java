b0798d382a5c7c57b9a760724b0cae9f606a761c
Greena Simon <sigreena@amazon.com>
2018-10-18 15:34:10 -0400

    Everything starts with Hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public void printHello(){
+        System.out.print("Hello World");
+    }
+    
+    public static void main(String[] args){
+        HelloWorld a = new HelloWorld();
+        a. printHello();
+    }
+}
