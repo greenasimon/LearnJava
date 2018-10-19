f349402d04ff15ef81b3d87207f2d6770def3196
Greena Simon <sigreena@amazon.com>
2018-10-19 12:47:13 -0400

    testing

--- Point.java
+++ Point.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Point{
+    private int x;
+    private int y;
+    public Point(int a, int b){
+        x = a;
+        y = b;
+    }
+    public int getX(){ return x; }
+    public int getY(){ return y; }
+    public double FindDistance(Point otherPt){
+        int dx = x - otherPt.getX();
+        int dy = x - otherPt.getY();
+        return Math.sqrt(dx*dx + dy*dy );
+    }
 
-public class Point {
+    public static void main(String[] args){
+        Point P1 = new Point(5,8);
+        Point P2 = new Point(9,15);
+        System.out.println(P1.FindDistance(P2));
+    }
 }
